6b521d0ef60b0f8abb687c120e25a9e702f3ac90
MetalTurtle <dekolis1218@gmail.com>
2020-10-29 10:25:45 -0700

    Complete assignment

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,46 @@
 public class Dog extends Pet {
     // Instance variable(s)
+    String breed;
 
 
     // Constructors
-    
+
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    public Dog() {
+        super();
+    }
 
     // makeNoise() method
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Woof!");
+    }
 
 
     // toString method
 
+    @Override
+    public String toString() {
+        return super.toString() + ", Breed: " + breed;
+    }
+
 
     // Getter
 
+    public String getBreed() {
+        return this.breed;
+    }
 
     // Setter
+
+    public void setBreed(String breed) {
+        if (breed.trim().length() != 0) {
+            this.breed = breed;
+        }
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,8 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog dog = new Dog("Fido", 2, "Dogdog");
+
+        dog.makeNoise();
+        System.out.println(dog);
     }
 }
